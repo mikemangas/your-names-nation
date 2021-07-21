e6b8c98639d041e9fe3a84a27f6bfd3ddc54759c
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-22 00:20:58 +0200

    add custom hook to gender page, but localstorage needed in custom hook

--- src/pages/Gender.js
+++ src/pages/Gender.js
@@ -1,7 +1,10 @@
+import useFetch from "../hooks/useFetch";
+
 export default function Gender() {
+  const { gender } = useFetch();
   return (
     <section className="gender">
-      <h2>Gender</h2>
+      <h2>Is your gender really {gender} ?</h2>
     </section>
   );
 }

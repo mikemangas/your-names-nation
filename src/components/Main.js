c1029f6e165f955edab8084d2527bc7ca0a44d12
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-21 22:53:27 +0200

    fetch data api and render output

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Gender from "../pages/Gender";
 import Nation from "../pages/Nation";
 import Age from "../pages/Age";
-import home from "../pages/Home";
 import Home from "../pages/Home";
-
-// import Home from "./pages/Home";
-// import Age from "./pages/Age";
-// import Nation from "./pages/Nation";
-// import Gender from "./pages/Gender";
 
 export default function Main() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,55 @@
+import React, { useState, useEffect } from "react";
+
 export default function Home() {
+  const [state, setState] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const url = `https://api.agify.io/?name=${state}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setAge(data.age));
+  });
+
+  useEffect(() => {
+    const url = `https://api.nationalize.io/?name=${state}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCountry(data.country[0]));
+  });
+
+  useEffect(() => {
+    const url = `https://api.genderize.io/?name=${state}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setGender(data.gender));
+  });
+
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formInputValue = form.input.value;
+    setState(formInputValue);
+  }
+
   return (
     <>
       <h2>Where is your name from?</h2>
-      <form className="submitForm">
-        <input placeholder="type your name" type="text"></input>
+      <form onSubmit={handleOnSubmit} className="submitForm">
+        <input
+          name="input"
+          id="input"
+          placeholder="type your name"
+          type="text"
+        ></input>
         <button type="submit">Explore</button>
       </form>
+      <p className="ageResult">Your Age is: {age}</p>
+      <p className="nameResult">Your Name is: {state}</p>
+      <p className="countryResult">You are from: {country}</p>
+      <p className="countryResult">Your Gender is: {gender}</p>
     </>
   );
 }

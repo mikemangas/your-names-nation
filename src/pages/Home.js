0fbe4bc3718a78a1f8e3e5f0b1a61df023c8fd0e
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-22 00:17:45 +0200

    create custom hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import useFetch from "../components/useFetch";
 
 export default function Home() {
-  const [state, setState] = useState("");
-  const [age, setAge] = useState("");
-  const [gender, setGender] = useState("");
-  const [country, setCountry] = useState([]);
-
-  useEffect(() => {
-    const url = `https://api.agify.io/?name=${state}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setAge(data.age));
-  });
-
-  useEffect(() => {
-    const url = `https://api.nationalize.io/?name=${state}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setCountry(data.country[0].country_id));
-  });
-
-  useEffect(() => {
-    const url = `https://api.genderize.io/?name=${state}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setGender(data.gender));
-  });
+  const [state, setState] = useState();
+  const { age, gender, country } = useFetch(state);
 
   function handleOnSubmit(event) {
     event.preventDefault();
     const form = event.target;
     const formInputValue = form.input.value;
     setState(formInputValue);
+    form.reset();
   }
-
   return (
-    <>
-      <h2>Where is your name from?</h2>
+    <div className="Home">
+      <h2>WHO - ARE - YOU</h2>
       <form onSubmit={handleOnSubmit} className="submitForm">
         <input
           name="input"
@@ -49,7 +27,7 @@ export default function Home() {
       <p className="ageResult">Your Age is: {age}</p>
       <p className="nameResult">Your Name is: {state}</p>
       <p className="countryResult">You are from: {country}</p>
-      <p className="countryResult">Your Gender is: {gender}</p>
-    </>
+      <p className="genderResult">Your Gender is: {gender}</p>
+    </div>
   );
 }

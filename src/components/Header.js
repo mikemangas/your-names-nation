6b7d7669ff3721260a8f8c8307c2985f4bf77ae1
Michail Mangasarov <mangasarov@hotmail.de>
2021-07-22 11:17:06 +0200

    remove fetchdata.js, add switch route to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="App__header">
-      <nav className="App__navigation">
-        <NavLink to="/">Logo</NavLink>
-      </nav>
+      <Switch>
+        <Route path="/gender">
+          <h1>What is your Gender?</h1>
+        </Route>
+        <Route path="/nation">
+          <h1>What is your Nation?</h1>
+        </Route>
+        <Route path="/age">
+          <h1>What is your Age?</h1>
+        </Route>
+        <Route path="/">
+          <h1>WHO - IS - YOUR - NAME</h1>
+        </Route>
+      </Switch>
     </header>
   );
 }
